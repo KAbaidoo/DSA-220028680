454f1fd33265f741c026870b951868d3f0036fb2
KAbaidoo <kabaidoo@hotmail.com>
2021-08-01 17:17:42 +0000

    Added javdoc

--- src/singlyLinkedList/iLinkedList.java
+++ src/singlyLinkedList/iLinkedList.java
@@ -1,29 +1,86 @@
 package singlyLinkedList;
 
 public interface iLinkedList {
+    /**
+     * Inserts the specified element at the beginning of this list.
+     *
+     * @param e The element to be inserted
+     */
+     void addFirst(int e);
 
-    public void addFirst(int e);
+    /**
+     * Appends the specified element to the end of this list.
+     *
+     * @param e    The element to be appended
+     * @param next Next node of list
+     */
+     void addLast(int e, LinkedList.Node next);
 
-    public void addLast(int e, LinkedList.Node next);
+    /**
+     * Appends the specified element to the end of this list.
+     *
+     * @param e    The element to be appended
+     * @param next Next node of list
+     */
+     void add(int e, LinkedList.Node next);
 
-    public void add(int e,  LinkedList.Node next) ;
+    /**
+     *  Inserts all of the elements in the specified collection into this list, starting at the specified position.
+     *
+     * @param arr Collection of items to be added
+     */
+     void addAll(int[] arr);
 
-    public void addAll(int[] arr);
+    /**
+     *  Returns the number of elements in this list.
+     *
+     * @return size of the list
+     */
+     int getSize();
 
-    public int getSize();
+    /**
+     * Returns the first element in this list.
+     *
+     * @return integer
+     */
+     int getFirst();
 
-    public int getFirst();
+    /**
+     * Returns the last element in this list.
+     *
+     * @return integer
+     */
+     int getLast();
 
-    public int getLast();
+    /**
+     *  Returns the element at the specified position in this list.
+     *
+     * @param index index of element to return
+     * @return integer
+     */
+     int get(int index);
 
-    public int get(int index);
+    /**
+     *  Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element.
+     *
+     * @param e element to search for
+     * @return index of first occurrence of the specified element or  -1 if this list does not contain the element.
+     */
+     int indexOf(int e);
 
-    public int indexOf(int e);
+    /**
+     *  Removes all of the elements from this list.
+     */
+     void clear();
 
-    public void clear();
+    /**
+     *  Removes and returns the first element from this list.
+     */
+     void removeFirst();
 
-    public void removeFirst();
-
-    public void removeLast();
+    /**
+     *  Removes and returns the first element from this list.
+     */
+     void removeLast();
 
 }
